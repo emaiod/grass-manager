a5432d0c76ced36a331f3e7300935e1604654323
emaiod <96419865+emaiod@users.noreply.github.com>
2026-07-14 20:50:51 +0200

    Start Grass Manager 0.4.1 backend

--- grass_manager/run.sh
+++ grass_manager/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv bashio
 set -e
 cd /app
-exec uvicorn main_v040:app --host 0.0.0.0 --port 8100 --proxy-headers --forwarded-allow-ips='*'
+exec uvicorn main_v041:app --host 0.0.0.0 --port 8100 --proxy-headers --forwarded-allow-ips='*'
