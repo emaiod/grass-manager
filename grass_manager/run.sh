78c2b6086d81e1dde8dcb75e36888105728cf2ab
emaiod <96419865+emaiod@users.noreply.github.com>
2026-07-14 20:27:18 +0200

    feat: bind Grass Manager portal to port 8100

--- grass_manager/run.sh
+++ grass_manager/run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/with-contenv bashio
 set -e
 cd /app
-exec uvicorn main:app --host 0.0.0.0 --port 8099 --proxy-headers --forwarded-allow-ips='*'
+exec uvicorn main:app --host 0.0.0.0 --port 8100 --proxy-headers --forwarded-allow-ips='*'
